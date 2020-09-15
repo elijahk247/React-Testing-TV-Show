49672f1721b078078958015c8d3320367a63e04f
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-09-15 14:29:36 -0400

    added the tests files, running into error when rendering the episodes

---
+++ src/tests/Episodes.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Episodes from '../components/Episodes';
+
+test('Renders the Episodes component', () => {
+  render(<Episodes episodes={{}} />)
+})
